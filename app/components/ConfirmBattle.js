302442b15629fd05e20f2fd0c10c348cc56f3f7c
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-07-08 12:18:20 -0400

    completion of app

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,12 +5,14 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function ConfirmBattle (props) {
   return props.isLoading === true
-    ? <p>LOADING</p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-        <h1>Prepare to Battle</h1>
+    ? <Loading />
+    : <MainContainer>
+        <h1>BATTLE</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
             <UserDetails info={props.playersInfo[0]} />
@@ -29,7 +31,7 @@ function ConfirmBattle (props) {
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,11 +1,39 @@
 var axios = require('axios');
 
-var id = "client"
-var sec = "secret"
-var param = "?client_id=" + id + "&client_secret" + sec;
+var id = "d30b15df334a216545a8"
+var sec = "03abb8f99a1f5ed3d2659a41a2e33b82c76beefb"
+var param = "?client_id=" + id + "&client_secret=" + sec;
 
 function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param);
+}
+
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=300');
+}
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores (players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
 }
 
 var helpers = {
@@ -17,7 +45,16 @@ var helpers = {
         return user.data;
       })
     }).catch(function(error) {
-      console.warn('Error in getPlayersInfo', err)})
+      console.warn('Error in getPlayersInfo', err)
+    })
+  },
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
